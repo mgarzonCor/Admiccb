4227427112e602da2c9a38de7c95bfb72d97f1b3
Michael Alejandro Garzon Urquijo <mgarzon@corparques.co>
2024-02-23 07:50:36 -0500

    rutas front

--- AdminCCB/routes/web.php
+++ AdminCCB/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/pdf', function () {
+    return view('pdf');
+});
+Route::get('/form', function () {
+    return view('formRegister');
+});
